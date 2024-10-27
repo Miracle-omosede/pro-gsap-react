6805f925408ef9f67c201a4e36c11aa62128a696
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-10-27 02:46:08 +0100

    pushed

---
+++ src/app/snippets/materials.jsx
@@ -0,0 +1,15 @@
+useEffect(() => {
+    Object.entries(materials).map((material) => {
+      // these are the material names that can't be changed color
+      if (
+        material[0] !== "zFdeDaGNRwzccye" &&
+        material[0] !== "ujsvqBWRMnqdwPx" &&
+        material[0] !== "hUlRcbieVuIiOXG" &&
+        material[0] !== "jlzuBkUzuJqgiAK" &&
+        material[0] !== "xNrofRCqOXXHVZt"
+      ) {
+        material[1].color = new THREE.Color(props.item.color[0]);
+      }
+      material[1].needsUpdate = true;
+    });
+  }, [materials, props.item]);
